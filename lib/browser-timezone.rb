eed2b1bbfed45206cc441dfb3b90c25624f187dd
Zach Millman <zach.millman@gmail.com>
2012-05-01 14:15:45 -0700

    Fix railtie, perhaps?

--- lib/browser-timezone.rb
+++ lib/browser-timezone.rb
@@ -1,6 +1,8 @@
 require "browser-timezone/version"
 
-require "browser-timezone/engine" # For javascript assets
+require "browser-timezone/engine" if defined?(Rails) # For javascript assets
 
 require "browser-timezone/instance_methods"
 require "browser-timezone/class_methods"
+
+require "browser-timezone/railtie" if defined?(Rails)
